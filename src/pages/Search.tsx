5179baa3813ed7c6887df0c8a60be02fb270ba9c
EdgarPetrosian <35850403+EdgarPetrosian@users.noreply.github.com>
2022-12-11 14:28:54 +0400

    Added debdebounced search hook

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,27 +1,28 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import ImageItem from '../components/ImageItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 import getGifsData from '../services/getGifsData';
 import { IImageItem } from "../types";
+import useDebounce from "../hooks/useDebounce";
 
 const Search = () => {
   const [search, setSearch] = useState<string>('Christmass')
   const [giphsData, setGipgsData] = useState([]);
   const [loading, setLoading] = useState(false)
 
+  const debouncedSearch = useDebounce(search, 500);
+
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
-      if (search?.length >= 3) {
-        const giphObject = await getGifsData(search);
-        setGipgsData(giphObject)
-      }
+      const giphObject = await getGifsData(debouncedSearch);
+      setGipgsData(giphObject)
       setLoading(false);
     }
-    fetchData();
-  }, [search])
+    if (debouncedSearch) fetchData();
+  }, [debouncedSearch])
 
-  const listItems = giphsData.map((data): IImageItem => <ImageItem key={data.images.original.hash} url={data.images.original.url} width={"500px"} height={"300px"}/>)
+  const listItems = giphsData.map((data): IImageItem => <ImageItem key={data.images.original.hash} url={data.images.original.url} width={"500px"} height={"300px"} />)
 
   return (
     <div className={"container"}>
