2195aa05d1fa9234c04b71f39f9a934245f0a663
EdgarPetrosian <35850403+EdgarPetrosian@users.noreply.github.com>
2022-12-11 13:09:23 +0400

    Created new screens, added navigation routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Header from './components/Header';
+
+import Home from './pages/Home';
+import Search from './pages/Search';
+
+import './Styles/App.css';
+import './Styles/search.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+       <Header/>
+        <Routes>
+          <Route exact path='/home' element={<Home />}></Route>
+          <Route exact path='/search' element={<Search />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
